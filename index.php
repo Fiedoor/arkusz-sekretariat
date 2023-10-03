94e838b8426afcf7a69a037b2a3736af72ded95b
Stanisław Fiedoruk <11120@kasprzak.edu.pl>
2023-10-03 14:32:53 +0200

    Html napisany

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Sekretariat</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+
+<body>
+    <div id="left">
+        <h1>Akta Pracownicze</h1>
+        <?php
+        //skrypt1
+        ?>
+        <hr>
+        <h3>Dokumenty pracownika</h3>
+        <a download="cv.txt">Pobierz</a>
+        <h1>Liczba zatrudnionych pracowników</h1>
+        <p>
+            <?php
+            //skrypt2
+            ?>
+        </p>
+    </div>
+    <div id="right">
+        <?php
+        //skrypt3
+        ?>
+    </div>
+    <footer>
+        Autorem aplikacji jest: Stanisław Fiedoruk 5J
+        <ul>
+            <li>skontaktuj się</li>
+            <li>poznaj naszą firmę</li>
+        </ul>
+    </footer>
+</body>
+
+</html>
